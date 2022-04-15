131d7ad353c53b020e7965ff64a7b094ae94f74e
Thefutureseer <danielharrislimos@outlook.com>
2022-04-15 15:40:38 -0700

    local refresh functionality

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
-var counter = document.querySelector("#counter");
-var addButton = document.querySelector("#add");
-var subtractButton = document.querySelector("#subtract");
+//Random number between 0-2 and get that index of the letters array
+//This will be the computers pick
+const randomNum = Math.floor(Math.random() * 10 % 3);
+console.log(randomNum, " random number");
 
-var count = localStorage.getItem("count");
+let lost = localStorage.setItem("losses", 0);
+let wins = localStorage.setItem("wins", 0);
 
-counter.textContent = count;
+//reach into local storage
+lost = localStorage.getItem("looses");
+wins = localStorage.getItem("wins");
 
-addButton.addEventListener("click", function() {
-  if (count < 24) {
-    count++;
-    counter.textContent = count;
-    localStorage.setItem("count", count);
+//Function to get computers random pick on page load
+function computerPick(randoNumo) {
+  const lettersArray = ["r", "p", "s"];
+
+ var compPicked = lettersArray[randoNumo]
+ console.log(compPicked, "comp picked!")
+ 
+ 
+ var conf = confirm(`so far you have ${lost} looses & ${wins} wins.`+"Ok to play?");
+ //add switch
+  if (conf) {
+    const myPick = prompt("pick r,p,s..");
+    if (myPick) {
+     if(compPicked === myPick) {
+      //  console.log("r");
+      //wins tick up once
+      //and set wins into local storage
+      wins++;
+      // localStorage.setItem("wins", wins);
+
+      alert("you choose wisely, lets see if you can do it again")
+      confirm(" you Win! Would you like to play again?");
+      //game loads 
+      computerPick(randomNum);
+     } else {
+      console.log('you lost');
+      //looses ticks up
+      //and put looses into local storage
+      lost++;
+      // localStorage.setItem("looses", lost);
+      // lost = localStorage.getItem("looses");
+      alert("bad gamble, lets see if you can win now!")
+
+      //confirm you lost
+      confirm("you lost, would you like to play again?");
+
+      //game loads agian
+      computerPick(randomNum);
+     }
+    } 
+  } else {
+    alert("see ya")
   }
-});
-
-subtractButton.addEventListener("click", function() {
-  if (count > 0) {
-    count--;
-    counter.textContent = count;
-    localStorage.setItem("count", count);
-  }
-});
+}
+//game loads on page load
+computerPick(randomNum);
