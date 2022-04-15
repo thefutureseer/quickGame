aa1c075a8fb9a4fd14a58d812ef7a470ae3291e1
Thefutureseer <danielharrislimos@outlook.com>
2022-04-15 15:38:45 -0700

    add refresh storage local

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
-//Random number between 0-2 and get that index of the letters array
-//This will be the computers pick
-const randomNum = Math.floor(Math.random() * 10 % 3);
-console.log(randomNum, " random number");
+var counter = document.querySelector("#counter");
+var addButton = document.querySelector("#add");
+var subtractButton = document.querySelector("#subtract");
 
-let lost = 0;
-let wins = 0;
+var count = localStorage.getItem("count");
 
-//Function to get computers random pick on page load
-function computerPick(randoNumo) {
-  const lettersArray = ["r", "p", "s"];
+counter.textContent = count;
 
- var compPicked = lettersArray[randoNumo]
- console.log(compPicked, "comp picked!")
- 
- 
- var conf = confirm(`so far you have ${lost} looses & ${wins} wins.`+" Ok to play ");
- //add switch
-  if (conf) {
-    const myPick = prompt("pick r,p,s..");
-    if (myPick) {
-     if(compPicked === myPick) {
-      //  console.log("r");
-      //wins tick up once
-      wins++;
-      alert("you choose wisely, lets see if you can do it again")
-      confirm(" you Win! Would you like to play again?");
-      //game loads 
-      computerPick(randomNum);
-     } else {
-      console.log('you lost');
-      //looses ticks up
-      lost++;
-      alert("bad gamble, lets see if you can win now!")
-
-      //confirm you lost
-      confirm("you lost, would you like to play again?");
-
-      //game loads agian
-      computerPick(randomNum);
-     }
-    } 
-  } else {
-    alert("see ya")
+addButton.addEventListener("click", function() {
+  if (count < 24) {
+    count++;
+    counter.textContent = count;
+    localStorage.setItem("count", count);
   }
-}
-//game loads on page load
-computerPick(randomNum);
+});
+
+subtractButton.addEventListener("click", function() {
+  if (count > 0) {
+    count--;
+    counter.textContent = count;
+    localStorage.setItem("count", count);
+  }
+});
