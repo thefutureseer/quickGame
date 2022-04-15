adb379d6c3dcbc075f1b25e705260d599fcada54
Thefutureseer <danielharrislimos@outlook.com>
2022-04-14 21:17:18 -0700

    add script and html

---
+++ script.js
@@ -0,0 +1,46 @@
+//Random number between 0-2 and get that index of the letters array
+//This will be the computers pick
+const randomNum = Math.floor(Math.random() * 10 % 3);
+console.log(randomNum, " random number");
+
+let lost = 0;
+let wins = 0;
+
+//Function to get computers random pick on page load
+function computerPick(randoNumo) {
+  const lettersArray = ["r", "p", "s"];
+
+ var compPicked = lettersArray[randoNumo]
+ console.log(compPicked, "comp picked!")
+ 
+ 
+ confirm(`so far you have ${lost} looses & ${wins} wins.`+"Do you want to play a game?");
+ //add switch
+  if (true) {
+    const myPick = prompt("pick r,p,s..");
+    if (myPick) {
+     if(compPicked === myPick) {
+      //  console.log("r");
+      //wins tick up once
+      wins++;
+      alert("you choose wisely, lets see if you can do it again")
+      confirm(" you Win! Would you like to play again?");
+      //game loads 
+      computerPick(randomNum);
+     } else {
+      console.log('you lost');
+      //looses ticks up
+      lost++;
+      alert("bad gamble, lets see if you can win now!")
+
+      //confirm you lost
+      confirm("you lost, would you like to play again?");
+
+      //game loads agian
+      computerPick(randomNum);
+     }
+    } 
+  }
+}
+//game loads on page load
+computerPick(randomNum);
